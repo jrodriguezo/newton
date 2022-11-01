f5540ab1be590edd6266428dfd95f01ba1ea1fa0
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-11-01 16:29:35 +0100

    feat: add login authentication with google

---
+++ src/utils/googleAuth.js
@@ -0,0 +1,33 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { app } from "../../firebase.config"
+
+async function googleAuth() {
+    console.log('login...')
+    const auth = getAuth(app)
+    const provider = new GoogleAuthProvider()
+
+    const response = await signInWithPopup(auth, provider)
+    
+    /* --- TODO: 
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result)
+        const token = credential.accessToken
+        // The signed-in user info.
+        const user = result.user
+        console.log(result)
+      }).catch((error) => {
+        // Handle Errors here.
+        // const errorCode = error.code
+        // const errorMessage = error.message
+        // The email of the user's account used.
+        // const email = error.customData.email
+        // The AuthCredential type that was used.
+        // const credential = GoogleAuthProvider.credentialFromError(error)
+      })
+      */
+     console.log(response);
+}
+
+export default googleAuth
