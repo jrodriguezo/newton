539118bcec75445f98b14df83732c87540ac4261
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-11-06 20:00:09 +0100

    fix: format with eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux'
 import { selectUser, setUser } from './redux/features/user/userSlice'
 import googleAuth from './utils/googleAuth'
 import './App.css'
 
-function App() {
+function App () {
   const dispatch = useDispatch()
-  const {user} = useSelector(selectUser)
+  const { user } = useSelector(selectUser)
 
   const handleLogin = async () => {
-    const userDetails = await googleAuth();
+    const userDetails = await googleAuth()
     const { user } = userDetails
     const { providerData } = user
     dispatch(setUser(providerData[0]))
     localStorage.setItem('user', JSON.stringify(providerData[0]))
   }
 
-  const handleLogout =  () => {
+  const handleLogout = () => {
     dispatch(setUser(null))
-    localStorage.removeItem('user');
+    localStorage.removeItem('user')
   }
 
   return (
     <div className="App">
       { user
-        ? 
-          <>
+        ? <>
             <img src={user.photoURL} alt={user.displayName} referrerPolicy="no-referrer" />
             <button onClick={handleLogout}>
               Logout
