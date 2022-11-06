19d05feb4aa0002710b14e5d4d4428026f206779
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-11-06 20:08:39 +0100

    fix: format with eslint

--- firebase.config.js
+++ firebase.config.js
@@ -1,14 +1,15 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
-const { 
-  VITE_FIREBASE_API_KEY, 
-  VITE_FIREBASE_AUTH_DOMAIN, 
-  VITE_FIREBASE_PROJECT_ID, 
-  VITE_FIREBASE_STORAGE_BUCKET, 
-  VITE_FIREBASE_MESSAGING_SENDER_ID, 
-  VITE_FIREBASE_APP_ID } = import.meta.env;
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID
+} = import.meta.env
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,9 +19,9 @@ const firebaseConfig = {
   storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: VITE_FIREBASE_APP_ID
-};
+}
 
-const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig)
 
 const firestore = getFirestore(app)
 
