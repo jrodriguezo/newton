33a6ac11b801eb0d41e5882a80df6bd1328ffff4
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-11-27 01:41:59 +0100

    refactor: change name

--- firebase.config.js
+++ firebase.config.js
@@ -23,8 +23,8 @@ const firebaseConfig = {
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig)
 
-const firestore = getFirestore(app)
+const db = getFirestore(app)
 
 const storage = getStorage(app)
 
-export { app, firestore, storage }
+export { app, db, storage }
