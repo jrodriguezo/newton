4d21d844513328915513b7afded94d9eadab4974
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-11-27 22:24:48 +0100

    feat: form is visible if user is logged

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,23 @@ import { db } from "../firebase.config";
 import Header from "./components/Header";
 import PersonalLiftings from "./components/PersonalLiftings";
 import Ranking from "./components/Ranking";
-import { setIsRankingUpdated } from "./redux/features/user/userSlice";
+import { COLORS } from "./constants/colors";
+import {
+  selectUser,
+  setIsRankingUpdated,
+} from "./redux/features/user/userSlice";
 
 function App() {
   const dispatch = useDispatch();
-  const isRankingUpdated = useSelector(prev => prev.user.isRankingUpdated)
+  const { user } = useSelector(selectUser);
+  const isRankingUpdated = useSelector((prev) => prev.user.isRankingUpdated);
   const [rankingData, setRankingData] = useState([]);
 
   const getRanking = async () => {
     const dbRef = collection(db, "ranking");
     const docSnap = await getDocs(dbRef);
     setRankingData(docSnap.docs);
-    dispatch(setIsRankingUpdated(false))
+    dispatch(setIsRankingUpdated(false));
   };
 
   useEffect(() => {
@@ -33,9 +38,15 @@ function App() {
   return (
     <div className="bg-slate-700 min-h-screen">
       <Header />
-      <div className="p-4">
+      <div className="flex flex-col gap-8 p-4">
         <Ranking rankingData={rankingData} />
-        <PersonalLiftings />
+        {user && (
+          <section>
+            <h1 className={`text-3xl ${COLORS.PURPLE}`}>New Record</h1>
+            <small className="block text-sm mb-4">Add your PR</small>
+            <PersonalLiftings />
+          </section>
+        )}
       </div>
     </div>
   );
