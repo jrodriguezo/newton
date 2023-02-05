ccdee187f3c171d64d22e341a6905093ed00bbd6
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2023-02-05 16:03:32 +0100

    feat: add aliases for paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+const aliasPrefix = '@'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // "@": path.resolve(__dirname, "./src/"),
+      [`${aliasPrefix}components`]: `${path.resolve(__dirname, "./src/components/")}`,
+      [`${aliasPrefix}constants`]: `${path.resolve(__dirname, "./src/constants/")}`,
+      [`${aliasPrefix}hooks`]: path.resolve(__dirname, "./src/hooks"),
+      [`${aliasPrefix}utils`]: `${path.resolve(__dirname, "./src/utils")}`,
+      [`${aliasPrefix}assets`]: `${path.resolve(__dirname, "./src/assets")}`,
+      [`${aliasPrefix}public`]: `${path.resolve(__dirname, "./public/")}`,
+    },
+  },
+});
